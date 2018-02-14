538ba7b5c61c739297ae3189fe6000d07ec82cfe
Hernan Zambrano <hernan@fuelpowered.com>
2018-02-14 11:25:24 -0800

    first commit

---
+++ RockPaperScisors/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+class RockPaperScissors
+{
+
+
+
+   
+	public static void Main (string[] args) {
+        RPSView view = new RPSView();
+        RPSController controller = new RPSController();
+        controller.StartGame();
+
+       
+	}
+}
